983deb26aca853aa254408f1578205efd753d318
Aminul Momin <A.Momin.NYC@gmail.com>
2026-03-08 18:32:32 -0500

    update py version to 3.13.2 loaclly

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # url="https://github.com/algorithms_and_data_structures",
     # download_url='https://github.com/...../..../',
     classifiers=[
-        'Programming Language :: Python :: 3.13.3',
+        'Programming Language :: Python :: 3.13.2',
     ],
     install_requires=[]
 )
